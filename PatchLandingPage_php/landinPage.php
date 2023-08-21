6f47a3c62871361fdc2c32adb11fdca75ff56b0e
Netta90 <antobrg90@hotmail.it>
2023-08-21 16:05:15 +0200

    creazioneDatabase

--- PatchLandingPage_php/landinPage.php
+++ PatchLandingPage_php/landinPage.php
@@ -1,4 +1,22 @@
 <?php
 
+$host= "127.0.0.1";
+$user="root";
+$password="";
 
-?>
+$connessione= new mysqli($host,$user,$password);
+var_dump($connessione);
+if($connessione===false){
+    die("CONNEcTION ERROR". $connessione->connect_error);}
+    
+
+
+    $sql="CREATE DATABASE PATCH_custumers";
+
+    $connessione->query($sql);
+
+
+
+    $connessione-> close();
+
+
